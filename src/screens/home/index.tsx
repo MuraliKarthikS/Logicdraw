ecd836b240ae653cf300b2c1efc7775a360c3b9f
MuraliKarthikS <murali.sasimollu2003@gmail.com>
2025-03-20 16:22:22 +0530

    draw feature added (only white color stroke at present)

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,8 +1,33 @@
 import { useEffect, useRef, useState } from "react";
+import { SWATCHES } from "@/constants";
+import { ColorSwatch,Group } from "@mantine/core";
+import {Button} from '@/components/ui/button';
+import axios from 'axios';
+
+interface Response {
+    exper: string;
+    result: string;
+    assign: boolean;
+}
+
+interface GeneratedResult {
+    expression: string;
+    answer: string;
+}
 
 export default function Home() {
     const canvasRef = useRef<HTMLCanvasElement>(null);
     const [isDrawing, setIsDrawing] = useState(false);
+    const [color, setColor] = useState('rgb(255, 255, 255)');
+    const [reset, setReset] = useState(false);
+    
+
+    useEffect(() => {
+        if (reset) {
+            resetCanvas();
+            setReset(false);
+        }
+    }, [reset]);
 
     useEffect(() => {
         const canvas = canvasRef.current;
@@ -18,6 +43,16 @@ export default function Home() {
     }
 }, []);
 
+    const resetCanvas = () => {
+        const canvas = canvasRef.current;
+        if (canvas) {
+            const ctx = canvas.getContext("2d");
+            if (ctx) {
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+            }
+        }
+    };
+
     const startDrawing = (e: React.MouseEvent<HTMLCanvasElement>) => {
         const canvas = canvasRef.current;
         if (canvas) {
@@ -26,7 +61,7 @@ export default function Home() {
             if (ctx) {
                 // ctx.lineWidth = 10;
                 // ctx.lineCap = "round";
-                // ctx.strokeStyle = "white";
+                // ctx.strokeStyle = color;
                 ctx.beginPath();
                 ctx.moveTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
                 setIsDrawing(true);
@@ -44,7 +79,7 @@ export default function Home() {
         if (canvas) {
             const ctx = canvas.getContext("2d");
             if (ctx) {
-                ctx.strokeStyle = "white";
+                ctx.strokeStyle = color;
                 ctx.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
                 ctx.stroke();
             }
